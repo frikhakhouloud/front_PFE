4439fb34a27cf71e2420395a9c37f0a71971f689
Khouloud <khouloudfrikha@gmail.com>
2023-03-29 10:40:28 +0100

    add component filtre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './composants/menu/menu.component';
-import { FiltreComponent } from './composants/filtre/filtre.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./composants/menu/menu.component";
+import { FiltreComponent } from "./composants/filtre/filtre.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    FiltreComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MenuComponent, FiltreComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/composants/filtre/filtre.component.ts
+++ src/app/composants/filtre/filtre.component.ts
@@ -1,8 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import axios from "axios";
 
 @Component({
-  selector: 'app-filtre',
-  templateUrl: './filtre.component.html',
-  styleUrls: ['./filtre.component.css'],
+  selector: "app-filtre",
+  templateUrl: "./filtre.component.html",
+  styleUrls: ["./filtre.component.css"],
 })
-export class FiltreComponent {}
+export class FiltreComponent implements OnInit {
+  divisions = [];
+  profits = [];
+  organisations = [];
+  weeks = [];
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getdivision();
+    this.getprofit();
+    this.getorganisation();
+    this.getweek();
+  }
+
+  getdivision() {
+    axios
+      .get("http://127.0.0.1:8000/division/")
+      .then((res) => {
+        console.log(res.data);
+        this.divisions = res.data.Results;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getprofit() {
+    axios
+      .get("http://127.0.0.1:8000/profit/")
+      .then((res) => {
+        console.log(res.data);
+        this.profits = res.data.Results;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getorganisation() {
+    axios
+      .get("http://127.0.0.1:8000/organisation/")
+      .then((res) => {
+        console.log(res.data);
+        this.organisations = res.data.Results;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getweek() {
+    axios
+      .get("http://127.0.0.1:8000/range/")
+      .then((res) => {
+        console.log(res.data);
+        this.weeks = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
